897e92f0acc304f24481a475a8bd7f3f5a98fc6b
Kostadin Terziev <kocyo.terziev@gmail.com>
2018-09-17 16:23:18 +0300

    Buy tokens with smart contract

--- contracts/KostaTokenSale.sol
+++ contracts/KostaTokenSale.sol
@@ -7,6 +7,9 @@ contract KostaTokenSale {
 	address admin;
 	KostaToken public tokenContract;
 	uint256 public tokenPrice;
+	uint256 public tokensSold;
+
+	event Sell(address _buyer, uint256 _amount);
 
 
 	constructor(KostaToken _tokenContract, uint256 _tokenPrice) public {
@@ -16,6 +19,28 @@ contract KostaTokenSale {
 		tokenContract = _tokenContract;
 		//Token Price
 		tokenPrice = _tokenPrice;
+
+	}
+
+	//multiply
+	function multiply(uint x, uint y) internal pure returns (uint z) {
+		require(y == 0 || (z - x * y) / y == x);
+	}
+
+	function buyTokens(uint256 _numberOfTokens) public payable {
+
+		//Require that value is equal to tokens
+		require(msg.value == multiply(_numberOfTokens, tokenPrice));
+		//Require that the contract has enough tokens
+		require(tokenContract.balanceOf(this) >= _numberOfTokens);
+		//Require that a transfer is successful
+		require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+		//Keep track of tokens sold 
+		tokensSold += _numberOfTokens;
+
+		//Trigger sell event
+		Sell(msg.sender, _numberOfTokens);
 
 	}
 }
--- test/KostaTokenSale.js
+++ test/KostaTokenSale.js
@@ -1,11 +1,17 @@
 var KostaTokenSale = artifacts.require("./KostaTokenSale.sol");
+var KostaToken = artifacts.require("./KostaToken.sol");
 
 
 contract('KostaTokenSale', function(accounts) {
 
 	var tokenSaleInstance;
+	var tokenInstance;
 	var tokenPrice = 1000000000000000;
-	  
+	var admin = accounts[0];
+	var buyer = accounts[1];
+	var tokensAvailable = 14700000;
+	var numberOfTokens;
+
 	it('initializes the contract with the correct values', function() {
 		return KostaTokenSale.deployed().then(function(instance) {
 			tokenSaleInstance = instance;
@@ -18,5 +24,38 @@ contract('KostaTokenSale', function(accounts) {
 			return tokenSaleInstance.tokenPrice();
 	}).then(function(price) {
 		assert.equal(price, tokenPrice, 'token price is correct');
+	});
+
+	it('facilitates token buying', function() {
+		return KostaTokenSale.deployed().then(function(instance) {
+			tokenInstance = instance;
+			return tokenSaleInstance.deployed();
+		}).then(function(instance) {
+			tokenSaleInstance = instance;
+			return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, { from: admin })
+		}).then(function(receipt) {
+			numberOfTokens = 10;
+			return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice })
+		}).then(function(receipt) {
+			assert.equal(receipt.logs.length, 1, 'triggers one event');
+		    assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+		    assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens');
+		    assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+			return tokenSaleInstance.tokensSold();
+		}).then(function(amount) {
+			assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold');
+			return tokenInstance.balanceOf(buyer);
+		}).then(function(balance) {
+			return tokenInstance.balanceOf(tokenSaleInstance.address);
+			assert.equal(balance.toNumber(), numberOfTokens);
+		}).then(function(balance) {
+			assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
+			return tokenSaleInstance.buyTokens(numberOfTokens, { 1 });
+		}).then(assert.fail).catch(function(error) {
+			assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+			return tokenSaleInstance.buyTokens( 30000000, { from: buyer, value: numberOfTokens * tokenPrice });
+		}).then(assert.fail).catch(function(error) {
+			assert(error.message.indexOf('revert') >= 0, 'cannot purchase more tokens than available');
+		});
 	});
 });
